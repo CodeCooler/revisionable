5c85b43ef3696414a4402d1111a0b0cb77410a73
CodeCooler Solutions LLC <CodeCooler@users.noreply.github.com>
2020-12-19 02:50:57 -0500

    Update RevisionableTrait.php

--- src/Venturecraft/Revisionable/RevisionableTrait.php
+++ src/Venturecraft/Revisionable/RevisionableTrait.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Arr;
 /*
  * This file is part of the Revisionable package by Venture Craft
  *
